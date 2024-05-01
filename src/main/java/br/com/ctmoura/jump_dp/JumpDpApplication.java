f50db7edac30d89f77fec31563b73f8a94f60d61
Cleber Moura <ctm@cin.ufpe.br>
2024-05-01 19:18:59 -0300

    adicionado projeto spring

---
+++ src/main/java/br/com/ctmoura/jump_dp/JumpDpApplication.java
@@ -0,0 +1,13 @@
+package br.com.ctmoura.jump_dp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumpDpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumpDpApplication.class, args);
+	}
+
+}
